2c02e8fc8794e1aef1c29552f0bc86c79ecafed5
dilushaeka <dilushaeka99@gmail.com>
2025-01-25 10:21:07 +0530

    completed recipe category API

--- src/main/java/com/sentura/bLow/controller/RecipeCategoryAPI.java
+++ src/main/java/com/sentura/bLow/controller/RecipeCategoryAPI.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 @RequestMapping("/api/v1/recipeCategory")
 public class RecipeCategoryAPI {
 
+
     @Autowired
     private RecipeCategoryService recipeCategoryService;
 
